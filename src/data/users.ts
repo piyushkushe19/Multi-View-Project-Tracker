71deb481085c33f2b4accf5db1e97b7bd28de0a3
Piyush Kushe <159922215+piyushkushe19@users.noreply.github.com>
2026-03-23 13:24:03 +0530

    Almost done..Some minor change to be done

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,12 +1,12 @@
 import type { User } from '../types';
 
 export const USERS: User[] = [
-  { id: 'u1', name: 'Ram Krshna',     initials: 'AK', color: '#6f5de7' },
-  { id: 'u2', name: 'Sham Singh',   initials: 'ST', color: '#1d9e75' },
-  { id: 'u3', name: 'Piyush Kushe',   initials: 'JL', color: '#e24b4a' },
-  { id: 'u4', name: 'Neel Raj', initials: 'CR', color: '#ef9f27' },
-  { id: 'u5', name: 'Monalisa Kulkarni',  initials: 'MC', color: '#378add' },
-  { id: 'u6', name: 'Sagar Hinge',   initials: 'RP', color: '#d4537e' },
+  { id: 'u1', name: 'Ram Krshna',     initials: 'RM', color: '#6f5de7' },
+  { id: 'u2', name: 'Sham Singh',   initials: 'SS', color: '#1d9e75' },
+  { id: 'u3', name: 'Piyush Kushe',   initials: 'PK', color: '#e24b4a' },
+  { id: 'u4', name: 'Neel Raj', initials: 'NR', color: '#ef9f27' },
+  { id: 'u5', name: 'Monalisa Kulkarni',  initials: 'MK', color: '#378add' },
+  { id: 'u6', name: 'Sagar Hinge',   initials: 'SH', color: '#d4537e' },
 ];
 
 export const getUserById = (id: string): User =>
